6452e4b1020cbbf1ea6e06e903ad46240463f0cc
Graham Watson <grahamj.watson@gmail.com>
2016-05-11 17:20:25 +0100

    First cut in progress TrackerSolution tracker class and tests

--- SudokuTest/TrackSolution.swift
+++ SudokuTest/TrackSolution.swift
@@ -10,59 +10,84 @@ import Foundation
 
 class TrackSolution: NSObject, NSCopying {
     
-    private var userSupplied: [(boardRows: Int, boardColumns: Int, cellRows: Int, cellColumns: Int)] = []
-    private var maxBoardRows: Int
-    private var maxBoardColumns: Int
-    private var maxCellRows: Int
-    private var maxCellColumns: Int
+    private var userSupplied: [(boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int)] = []
+    private var boardSize: (maxBoardRows: Int, maxBoardColumns: Int, maxCellRows: Int, maxCellColumns: Int) = (-1, -1, -1, -1)
     
-    init(boardRows: Int, boardColumns: Int, cellRows: Int, cellColumns: Int) {
-        self.maxBoardRows = boardRows
-        self.maxBoardColumns = boardColumns
-        self.maxCellRows = cellRows
-        self.maxCellColumns = cellColumns
+    init(boardRows: Int = 3, boardColumns: Int = 3, cellRows: Int = 3, cellColumns: Int = 3) {
+        self.boardSize.maxBoardRows = boardRows
+        self.boardSize.maxBoardColumns = boardColumns
+        self.boardSize.maxCellRows = cellRows
+        self.boardSize.maxCellColumns = cellColumns
         return
     }
     
-    func addCoordinateToSolution(boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int) {
+    // only add the coord if in bounds of the board we're tracking and it hasn't already been added
+    func addCoordinate(boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int) -> Int {
         if boardRow < 0 || boardColumn < 0 || cellRow < 0 || cellColumn < 0 {
-            return
+            return -1
         }
-        if boardRow >= self.maxBoardRows || boardColumn >= self.maxBoardColumns || cellRow >= self.maxCellRows || cellColumn >= self.maxCellColumns {
-            return
+        if boardRow >= self.boardSize.maxBoardRows || boardColumn >= self.boardSize.maxBoardColumns || cellRow >= self.boardSize.maxCellRows || cellColumn >= self.boardSize.maxCellColumns {
+            return -1
+        }
+        if self.getCoordinateIndex(boardRow, boardColumn: boardColumn, cellRow: cellRow, cellColumn: cellColumn) > -1 {
+            return -1
         }
         userSupplied.append((boardRow, boardColumn, cellRow, cellColumn))
-        return
+        return self.userSupplied.count
     }
 
-    func removeCoordinateFromSolution(boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int) {
-        if boardRow < 0 || boardColumn < 0 || cellRow < 0 || cellColumn < 0 {
-            return
-        }
-        if boardRow >= self.maxBoardRows || boardColumn >= self.maxBoardColumns || cellRow >= self.maxCellRows || cellColumn >= self.maxCellColumns {
-            return
-        }
-        for index: Int in 0 ..< self.userSupplied.count {
-            if userSupplied[index].boardRows == boardRow && userSupplied[index].boardColumns == boardColumn && userSupplied[index].cellRows == cellRow && userSupplied[index].cellColumns == cellColumn {
-                userSupplied.removeAtIndex(index)
-            }
-        }
+    func clearCoordinates() {
+        self.userSupplied.removeAll()
         return
     }
-
+    
     func countOfUserSolution() -> Int {
         return userSupplied.count
     }
+
+    func getCoordinateAtIndex(index: Int) -> (boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int) {
+        if index > self.countOfUserSolution() - 1 {
+            return(-1, -1, -1, -1)
+        }
+        return userSupplied[index]
+    }
+    
+    func getCoordinateIndex(boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int) -> Int {
+        for index: Int in 0 ..< self.countOfUserSolution() - 1 {
+            if userSupplied[index].boardRow == boardRow && userSupplied[index].boardColumn == boardColumn && userSupplied[index].cellRow == cellRow && userSupplied[index].cellColumn == cellColumn {
+                return index
+            }
+        }
+        return -1
+    }
+    
+    func getBoardSize() -> (maxBoardRows: Int, maxBoardColumns: Int, maxCellRows: Int, maxCellColumns: Int) {
+        return boardSize
+    }
+    
+    // remove the coord only if in bounds of the board and it's been stored
+    func removeCoordinate(boardRow: Int, boardColumn: Int, cellRow: Int, cellColumn: Int) -> Int {
+        if boardRow < 0 || boardColumn < 0 || cellRow < 0 || cellColumn < 0 {
+            return -1
+        }
+        if boardRow >= self.boardSize.maxBoardRows || boardColumn >= self.boardSize.maxBoardColumns || cellRow >= self.boardSize.maxCellRows || cellColumn >= self.boardSize.maxCellColumns {
+            return -1
+        }
+        for index: Int in 0 ..< self.countOfUserSolution() - 1 {
+            if userSupplied[index].boardRow == boardRow && userSupplied[index].boardColumn == boardColumn && userSupplied[index].cellRow == cellRow && userSupplied[index].cellColumn == cellColumn {
+                userSupplied.removeAtIndex(index)
+                return self.userSupplied.count
+            }
+        }
+        return -1
+    }
     
     func copyWithZone(zone: NSZone) -> AnyObject {
-        let copy = TrackSolution(boardRows: self.maxBoardRows, boardColumns: self.maxBoardColumns, cellRows: self.maxCellRows, cellColumns: self.maxCellColumns)
+        let copy = TrackSolution(boardRows: self.boardSize.maxBoardRows, boardColumns: self.boardSize.maxBoardColumns, cellRows: self.boardSize.maxCellRows, cellColumns: self.boardSize.maxCellColumns)
         for coords in self.userSupplied {
             copy.userSupplied.append(coords)
         }
-        copy.maxBoardRows = self.maxBoardRows
-        copy.maxBoardColumns = self.maxBoardColumns
-        copy.maxCellRows = self.maxCellRows
-        copy.maxCellColumns = self.maxCellColumns
+        copy.boardSize = self.boardSize
         return copy
     }
     
